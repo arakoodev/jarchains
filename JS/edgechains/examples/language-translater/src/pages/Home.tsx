8e70dc2c77640be4fabca7189049f4c45be724b6
shyam-raghuwanshi <sr9993663832@gmail.com>
2024-06-13 16:16:53 +0530

    fix-minor-issue

--- JS/edgechains/examples/language-translater/src/pages/Home.tsx
+++ JS/edgechains/examples/language-translater/src/pages/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
                 <div className="flex h-full justify-center items-center">
                     <div className="bg-white p-5 rounded-md shadow-md w-[40rem]">
 
-                        <form hx-post="/translate" hx-encoding="multipart/form-data" hx-swap="outerHTML" hx-target="#translatedText">
+                        <form hx-post="/translate" hx-encoding="multipart/form-data" hx-target="#translatedText">
 
                             <label htmlFor="language" className="block text-sm font-medium text-gray-700" >Language</label>
                             <select className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" name="language" hx-get="/models" hx-indicator=".htmx-indicator">
@@ -18,20 +18,23 @@ export default function Home() {
 
                             <label htmlFor="text" className="block text-sm font-medium text-gray-700 mt-5">Text to Translate</label>
                             <textarea
+                                id="textarea"
                                 className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                 name="text"
                                 rows={4}
-                                value={""}
                                 placeholder="Search..."
-                                _="on change my.value = value"
                             ></textarea>
-                            <button className="w-full bg-blue-500 text-white py-2 px-4 mt-5 rounded-md hover:bg-blue-700" _="on click call alert('You clicked me!')" type="submit">Submit</button>
+                            <button
+                                className="w-full bg-blue-500 text-white py-2 px-4 mt-5 rounded-md hover:bg-blue-700" 
+                                _="on click if #textarea.value == '' call alert('fill all the fields') " type="submit">Submit</button>
                         </form>
 
                         <div
                             id="translatedText"
                             className="mt-4 p-2 bg-gray-100 border border-gray-300 rounded-md"
-                        ></div>
+                        >
+
+                        </div>
                     </div>
                 </div>
             </Layout >

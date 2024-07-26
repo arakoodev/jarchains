ee42816f35350a8e7295958d45f773932c7c69f9
Shyam Raghuwanshi <sr9993663832@gmail.com>
2024-07-26 23:28:00 +0530

    Adding rpa example (#402)
    * adding rap challenge example
    
    * adding readme

---
+++ JS/edgechains/examples/rpa-challenge/src/lib/playwright.ts
@@ -0,0 +1,12 @@
+import { Playwright } from "@arakoodev/edgechains.js/scraper";
+
+async function call({ task, openai }: { task: string, openai: string }) {
+    const scraper = new Playwright({ apiKey: openai });
+    try {
+        return await scraper.call({ task, headless: false });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = call;
